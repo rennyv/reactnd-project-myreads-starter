91c96d5b9bae90d454b6fa52d29d3da09f8fc9e9
Renny Vanneste <renny@vanneste.ca>
2017-07-05 21:38:57 -0600

    moved book into shelf so I can use it on the search page

--- src/Shelf.js
+++ src/Shelf.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react'
-
+import Book from './Book.js'
 
 class Shelf extends Component {
   render() {
     const { title, books } = this.props
-    console.log(books)
+
     return (
       <div className="bookshelf">
         <h2 className="bookshelf-title">{ title }</h2>
@@ -12,22 +12,7 @@ class Shelf extends Component {
           <ol className="books-grid">
               {books.map((book) => (
                 <li key={book.id}>
-                  <div className="book">
-                    <div className="book-top">
-                      <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
-                      <div className="book-shelf-changer">
-                        <select>
-                          <option value="none" disabled>Move to...</option>
-                          <option value="currentlyReading">Currently Reading</option>
-                          <option value="wantToRead">Want to Read</option>
-                          <option value="read">Read</option>
-                          <option value="none">None</option>
-                        </select>
-                      </div>
-                    </div>
-                    <div className="book-title">{ book.title }</div>
-                    {book.authors.map((author, index) => (<div key={index} className="book-authors">{ author }</div>))}
-                  </div>
+                  <Book book={book} />
                 </li>
               ))}
           </ol>
