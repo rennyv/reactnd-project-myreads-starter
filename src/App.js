5d8bac003744581da20ac153470e811f6313d551
Renny Vanneste <renny@vanneste.ca>
2017-07-05 21:31:45 -0600

    created shelf

--- src/App.js
+++ src/App.js
@@ -1,18 +1,26 @@
 import React from 'react'
 import { Route } from 'react-router-dom'
-//import * as BooksAPI from './BooksAPI'
+import * as BooksAPI from './BooksAPI'
 import SearchBook from './SearchBook'
 import BookList from './BookList'
 import './App.css'
 
 class BooksApp extends React.Component {
-  state = {}
+  state = {
+    books: []
+  }
+
+  componentDidMount() {
+    BooksAPI.getAll().then((books) => {
+      this.setState({books})
+    })
+  }
 
   render() {
     return (
       <div className="app">
         <Route exact path="/" render={() => (
-          <BookList /> 
+          <BookList books={ this.state.books } /> 
         )}/>
         <Route path="/search" render={({ history }) => (
           <SearchBook />
