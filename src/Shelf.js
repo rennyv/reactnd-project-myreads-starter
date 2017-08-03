23015e3f2152fcdeb443ac8cb90751d6039ec0fb
Renny Vanneste <renny@vanneste.ca>
2017-08-03 07:12:55 -0600

    converted shelf to a function

--- src/Shelf.js
+++ src/Shelf.js
@@ -1,35 +1,32 @@
-import React, { Component } from 'react'
+import React from 'react'
 import Book from './Book.js'
 import PropTypes from 'prop-types'
 
-class Shelf extends Component {
-  static propTypes = {
-      title: PropTypes.string.isRequired,
-      books: PropTypes.array.isRequired,
-      onUpdateBook: PropTypes.func.isRequired
-  }
-
-  render() {
-    const { title, books, onUpdateBook } = this.props
-
-    return (
-      <div className="bookshelf">
-        <h2 className="bookshelf-title">{ title }</h2>
-        <div className="bookshelf-books">
-          <ol className="books-grid">
-              {books.map((book) => (
-                <li key={ book.id + "-" + book.title + "-" + book.shelf}>
-                  <Book 
-                    book={ book }
-                    onUpdateBook={ onUpdateBook }
-                  />
-                </li>
-              ))}
-          </ol>
-        </div>
-      </div>
-      )
-  }
+Shelf.propTypes = {
+  title: PropTypes.string.isRequired,
+  books: PropTypes.array.isRequired,
+  onUpdateBook: PropTypes.func.isRequired
 }
 
-export default Shelf
+export default function Shelf(props){
+  const { title, books, onUpdateBook } = props
+
+  return (
+    <div className="bookshelf">
+      <h2 className="bookshelf-title">{ title }</h2>
+      <div className="bookshelf-books">
+        <ol className="books-grid">
+            {books.map((book) => (
+              <li key={ book.id + "-" + book.title + "-" + book.shelf}>
+                <Book 
+                  book={ book }
+                  onUpdateBook={ onUpdateBook }
+                />
+              </li>
+            ))}
+        </ol>
+      </div>
+    </div>
+  )
+}
+
