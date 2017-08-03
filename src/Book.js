ef4ea04e4eec54a8b70d6b06dfedf43f99139289
Renny Vanneste <renny@vanneste.ca>
2017-08-02 21:17:51 -0600

    moved static css from inline to global css

--- src/Book.js
+++ src/Book.js
@@ -19,7 +19,7 @@ class Book extends Component {
     return (
       <div className="book">
         <div className="book-top">
-          <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
+          <div className="book-cover" style={{ backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
           <div className="book-shelf-changer">
           <select onChange={this.onSelectChange} defaultValue={book.shelf}>
             <option value="none" disabled>Move to...</option>
